b35c05fb05d472c6c1857c88d2bc88e64f4015c5
deepthi-dr <deepthi-dr@pluralsight.com>
2024-05-10 18:01:12 +0530

    added html with javascript

--- scripts/hellow-world.js
+++ scripts/hellow-world.js
@@ -11,3 +11,19 @@ function showGreeting() {
 // call the showGreeting() function when the .js
 // file loads and this line is encountered
 showGreeting();
+
+// We want this code to run when the user clicks the Show button
+
+let nameField = document.getElementById("nameField");
+let ageField = document.getElementById("ageField");
+
+// Get values in the name and age fields
+let name1 = nameField.value;
+let age =  ageField.value;   	// age is a string here
+
+let message = 
+  "Hi " + name1 + "! I hear you are " + age + " years old!";
+
+const messagePara = document.getElementById("messagePara");
+messagePara.innerHTML = message;
+
